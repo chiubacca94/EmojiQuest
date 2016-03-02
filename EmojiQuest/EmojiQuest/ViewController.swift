6ec65727584882b539834316acf12f679e0de181
bcc5160 <bc.chiu12@gmail.com>
2016-03-02 18:08:36 -0500

    Initial commit

---
+++ EmojiQuest/EmojiQuest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiQuest
+//
+//  Created by Brittany Chiu on 3/2/16.
+//  Copyright © 2016 IST446. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
