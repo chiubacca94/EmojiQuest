5e8fb68bba64a1ee497a3cde8845812344582a55
bcc5160 <bc.chiu12@gmail.com>
2016-04-14 17:14:44 -0400

    added inventory class (still need descriptions)

--- EmojiQuest/EmojiQuest/Item.swift
+++ EmojiQuest/EmojiQuest/Item.swift
@@ -9,5 +9,60 @@
 import Foundation
 
 class Item {
-    var Name : String?
+//    var Name : String?
+    
+    static let sharedInstance = Item()
+    
+    var itemDictionary: [String:Int] = [
+        "coin" : 0,
+        "lock pick" : 1
+    ]
+    
+    var items = [Int]()
+    
+    // Print Items in inventory (description)
+    func getItems(item:String){
+        print("Your items:\n")
+        
+        for(myKey, myValue) in itemDictionary{
+            print("\(myKey) - \(items[myValue])\n")
+        }
+    }
+    
+    // Add an item
+    func addItem(item:String){
+        // Check if there is actually an item in the game's item database
+        if(itemDictionary[item] == nil){
+            print("Nothing added because it doesn't exist!")
+        }
+        else{
+            if let newItem = itemDictionary[item] {
+                items[newItem] += 1
+            }
+        }
+    }
+    
+    
+    // Remove an item
+    func removeItem(item:String){
+        
+        // Check if there is actually an item in the game's item database
+        if(itemDictionary[item] == nil){
+            print("Nothing removed because it doesn't exist!")
+        }
+        else{
+           if let rmItem = itemDictionary[item] {
+            
+                // Check if there are any items in the inventory
+                if(items[rmItem] < 1){
+                    print("Nothing removed because you don't have any \(items[rmItem]) \n")
+                }
+                else{
+                    items[rmItem] -= 1;
+                }
+                
+            }
+        }
+    }
+
 }
