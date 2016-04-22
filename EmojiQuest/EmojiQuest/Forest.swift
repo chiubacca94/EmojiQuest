36a243c61fa6f30690cc29a82f5e565d28dd08f4
CarpeDiEmma <EyesOnly@client-130-203-114-178.mobility.psu.edu>
2016-04-22 10:17:04 -0400

    Intro text for forest

--- EmojiQuest/EmojiQuest/Forest.swift
+++ EmojiQuest/EmojiQuest/Forest.swift
@@ -19,10 +19,11 @@ class Forest {
     }
     
     func introductoryText() -> String {
-        return "You wake up in the middle of the woods. Your head hurts and your throat is sore. In front of you is a giant beast of a man, eating lunch. There are trees in every direction."
+        return "You awake to a rough hand shaking you. What? Where are you? Oh god, you could have been unconscious for days, maybe weeks, while that wizardly villain parades around in the skin of your King! \n\n But who is this ruffian, who shakes you out of your stupor? An ally, or perhaps an agent of the Wizard? You open your mouth to demand answers and… \n\n “....⁉️” Nothing comes out 😲 ‼️ Drat, that villain has stolen your voice, such that you may never expose his foul secret! \n\n The man who woke you, clearly a woodsman by dress, says “....are you alright ❔"
     }
     
     func parseEmoji(playerResponse: String) -> String {
+        // Check if text
         return woodsman.respondTo(playerResponse)
     }
 }
